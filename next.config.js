ee592bf23b461d0e78af23ed32134ec3b36fd5e1
muftsabazaar <muftsabazaar@gmail.com>
2021-12-24 21:41:13 +0530

    Change Logo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['muftsabazaar.com', 'lh3.googleusercontent.com'],
+    domains: ['muftsabazaar.com', 'lh3.googleusercontent.com', "res.cloudinary.com"],
   },
 }
--- src/Layout.js
+++ src/Layout.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import AppBar from '@mui/material/AppBar';
@@ -25,18 +25,18 @@ import Image from "next/image"
 
 const drawerWidth = 240;
 
-const errorIcon = () =>{
-  return(
+const errorIcon = () => {
+  return (
     <>
-    <div className="error-div-sidebar">
-    <CloudOffIcon sx={{fontSize:80}}/>
-    </div>
-    <p className="error-div-sidebar bottom-error">Please login first</p>
+      <div className="error-div-sidebar">
+        <CloudOffIcon sx={{ fontSize: 80 }} />
+      </div>
+      <p className="error-div-sidebar bottom-error">Please login first</p>
     </>
   )
 }
 
-export default function Layout({data, isLoading, children}, props) {
+export default function Layout({ data, isLoading, children }, props) {
   const [isSigned, setIsSigned] = React.useState(false);
   const [userData, setUserData] = React.useState([]);
   const [anchorElProfileMenu, setanchorElProfileMenu] = React.useState(null);
@@ -58,152 +58,152 @@ export default function Layout({data, isLoading, children}, props) {
     }
   });
 
-    const { window } = props;
-    const [mobileOpen, setMobileOpen] = React.useState(false);
- 
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
-      };
-      const container = window !== undefined ? () => window().document.body : undefined;
-      const [dark, setDark] = React.useState(false)
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-      let theme = createTheme({
-          palette: {
-              type: dark ? 'dark' : 'light',
-              mode: dark ? 'dark' : 'light',
-          },
-          typography: {
-            h1: {
-              fontSize: "3rem",
-              fontWeight: 550,
-            },
-          }
-      });
-      theme = responsiveFontSizes(theme);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  const container = window !== undefined ? () => window().document.body : undefined;
+  const [dark, setDark] = React.useState(false)
 
-      const HandleThemeChange = () =>{
-        setDark(!dark);
-      }
-  
-      const Signout = () => {
-        signOut(authorization).then(() => {
-          setIsSigned(false);
-          Router.push("/login")
-          handleProfileMenuClose();
-        }).catch((error) => {
-            // An error happened.
-        });
+  let theme = createTheme({
+    palette: {
+      type: dark ? 'dark' : 'light',
+      mode: dark ? 'dark' : 'light',
+    },
+    typography: {
+      h1: {
+        fontSize: "3rem",
+        fontWeight: 550,
+      },
     }
+  });
+  theme = responsiveFontSizes(theme);
+
+  const HandleThemeChange = () => {
+    setDark(!dark);
+  }
+
+  const Signout = () => {
+    signOut(authorization).then(() => {
+      setIsSigned(false);
+      Router.push("/login")
+      handleProfileMenuClose();
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
 
   return (
     <ThemeProvider theme={theme}>
       <Paper>
-    
-    <CssBaseline />
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-    {isLoading && <LinearProgress />}
-        <Toolbar>
-        <Box display='flex' flexGrow={1}>
-        <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-            <Link href="/" >
-              <>
-            <Image
-      src="https://muftsabazaar.com/blog/image/fulllogo.svg"
-      alt="Logo"
-      width={200}
-      height={55}
-    />
-    <div className="logo-text">
-    GeM Portal Course
-    </div>
-    </>
-           </Link>
-          </Box>
-         {isSigned? (
-           <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleProfileMenu}
-                color="inherit"
-              >
-                <Avatar alt={userData.displayName} src={userData.photoURL} sx={{ width: 35, height: 35 }}/>
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElProfileMenu}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorElProfileMenu)}
-                onClose={handleProfileMenuClose}
-              >
-                <MenuItem onClick={handleProfileMenuClose}>Profile</MenuItem>
-                <MenuItem onClick={Signout}>Logout</MenuItem>
-              </Menu>
-              </div>
-          ):<Link href="/login"><Button variant="contained">Login</Button></Link>}
 
-<Switch  handle={HandleThemeChange}/>
-        </Toolbar>
-      </AppBar>
-      
-      <Box sx={{ display: 'flex' }}>
-      <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          <Toolbar />
-        <Box sx={{ overflow: 'auto' }}>
-          {isSigned?<Sidebar data={data} handleDrawerToggle = {handleDrawerToggle}/>:errorIcon()}
-        
+        <CssBaseline />
+        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+          {isLoading && <LinearProgress />}
+          <Toolbar>
+            <Box display='flex' flexGrow={1}>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, display: { sm: 'none' } }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Link href="/" >
+                <a className="header-logo" >
+                <Image
+                  src="https://res.cloudinary.com/dl3tfsbn5/image/upload/ar_1:1,b_rgb:1976d2,bo_0px_solid_rgb:ff0000,c_fill,g_auto,h_93,r_max,w_99/v1640360177/fullogo.png"
+                  alt="Logo"
+                  width={35}
+                  height={35}
+                />
+                <span className="logo-text">
+                GeM Portal Course
+                </span>
+                </a>
+                 </Link>
+            </Box>
+            {isSigned ? (
+              <div>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleProfileMenu}
+                  color="inherit"
+                >
+                  <Avatar alt={userData.displayName} src={userData.photoURL} sx={{ width: 35, height: 35 }} />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElProfileMenu}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorElProfileMenu)}
+                  onClose={handleProfileMenuClose}
+                >
+                  <MenuItem onClick={handleProfileMenuClose}>Profile</MenuItem>
+                  <MenuItem onClick={Signout}>Logout</MenuItem>
+                </Menu>
+              </div>
+            ) : <Link href="/login"><Button variant="contained">Login</Button></Link>}
+
+            <Switch handle={HandleThemeChange} />
+          </Toolbar>
+        </AppBar>
+
+        <Box sx={{ display: 'flex' }}>
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
+          >
+            <Toolbar />
+            <Box sx={{ overflow: 'auto' }}>
+              {isSigned ? <Sidebar data={data} handleDrawerToggle={handleDrawerToggle} /> : errorIcon()}
+
+            </Box>
+          </Drawer>
+          <Drawer
+            variant="permanent"
+            sx={{
+              display: { xs: 'none', sm: 'block' },
+              width: drawerWidth,
+              flexShrink: 0,
+              [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+            }}
+          >
+            <Toolbar />
+            <Box sx={{ overflow: 'auto' }}>
+              {isSigned ? <Sidebar data={data} /> : errorIcon()}
+            </Box>
+          </Drawer>
+          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            <Toolbar />
+            {children}
+          </Box>
         </Box>
-        </Drawer>
-      <Drawer
-        variant="permanent"
-        sx={{
-            display: { xs: 'none', sm: 'block' },
-          width: drawerWidth,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
-        }}
-      >
-        <Toolbar />
-        <Box sx={{ overflow: 'auto' }}>
-        {isSigned?<Sidebar data={data} />:errorIcon()}
-        </Box>
-      </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Toolbar />
-       {children}
-      </Box>
-    </Box>
-    </Paper>
+      </Paper>
     </ThemeProvider>
   );
 }
