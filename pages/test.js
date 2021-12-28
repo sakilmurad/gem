04e0141c52b7200fa437651b8a908aee860f2aba
muftsabazaar <82711809+muftsabazaar@users.noreply.github.com>
2021-12-28 21:55:06 +0530

    Update test.js
    New test

--- pages/test.js
+++ pages/test.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Test() {
     const data = process.env.TEST
+    const newData = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
     return (
         <div>
             {data}
+        {newData}
         </div>
     )
 }
