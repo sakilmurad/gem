cf99ea46e49ac6c98e7b077d0f89b878265eeafc
muftsabazaar <muftsabazaar@gmail.com>
2021-12-29 21:55:20 +0530

    Add new post

--- pages/about-us.js
+++ pages/about-us.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div>
             We are not any official GeM partners. We only provide data as per our research and knowledge. Please be aware with your GeM account.
-            Don't give your credentials to anybody. Be safe and support our work if you like.
+            Don&apos;t give your credentials to anybody. Be safe and support our work if you like.
         </div>
     )
 }
