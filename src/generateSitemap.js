420c7d772467014095eb63449cf250273453143a
muftsabazaar <muftsabazaar@gmail.com>
2022-02-17 21:34:05 +0530

    Change inline css

--- src/generateSitemap.js
+++ src/generateSitemap.js
@@ -1,14 +1,10 @@
-const fs = require('fs')
+const fs = require("fs");
 const domain = "https://gpc.edafter.com";
-let data = JSON.parse(fs.readFileSync('src/data.json', 'utf-8'))
+let data = JSON.parse(fs.readFileSync("src/data.json", "utf-8"));
 
 const defaultPages = `
 <url>
-<loc>${domain}/about-us</loc>
-<lastmod>2022-01-24T17:48:29.837Z</lastmod>
-</url>
-<url>
-<loc>${domain}/contact</loc>
+<loc>${domain}</loc>
 <lastmod>2022-01-24T17:48:29.837Z</lastmod>
 </url>
 <url>
@@ -16,28 +12,43 @@ const defaultPages = `
 <lastmod>2022-01-24T17:48:29.837Z</lastmod>
 </url>
 <url>
-<loc>${domain}/make-in-india</loc>
+<loc>${domain}/signin</loc>
 <lastmod>2022-01-24T17:48:29.837Z</lastmod>
 </url>
 <url>
-<loc>${domain}/signin</loc>
+<loc>${domain}/pricing</loc>
+<lastmod>2022-01-24T17:48:29.837Z</lastmod>
+</url>
+<url>
+<loc>${domain}/refund-policy</loc>
+<lastmod>2022-01-24T17:48:29.837Z</lastmod>
+</url>
+<url>
+<loc>${domain}/tools/make-in-india</loc>
+<lastmod>2022-01-24T17:48:29.837Z</lastmod>
+</url>
+<url>
+<loc>${domain}/tools/reseller-authority-letter</loc>
+<lastmod>2022-01-24T17:48:29.837Z</lastmod>
+</url>
+<url>
+<loc>${domain}/tools/bidder-financial-standing</loc>
 <lastmod>2022-01-24T17:48:29.837Z</lastmod>
 </url>
 `;
 
-const generatedData = data.map(data => {
-  let slug = `/${data.slug}`;
-  if(data.slug == "/"){
-     slug = "";
-  }
-    return (
-        `<url>
+const generatedData = data
+  .map((data) => {
+    let slug = `/${data.slug}`;
+    if (data.slug == "/") {
+      slug = "";
+    }
+    return `<url>
     <loc>${domain}${slug}</loc>
     <lastmod>${data.created}</lastmod>
-  </url>`
-    )
-
-}).join(" ");
+  </url>`;
+  })
+  .join(" ");
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
